6b38b33c6eb144003e1a3038e12dd4ea1e65477e
Martin Pascal Schmauch <mschmauc@mail.uni-mannheim.de>
2021-03-26 15:59:59 +0100

    #29 Adding documentation to the FileParameters file in gui package

--- src/gui/FileParameters.java
+++ src/gui/FileParameters.java
@@ -1,11 +1,17 @@
 package gui;
 
-//** @author mschmauc
+// ** @author mschmauc
+
+/**
+ * This class supposed to ensure full file path functionality across all devices by using the
+ * file.separator property. File paths for all external resource files used for the GUI are saved in
+ * this class as public variable
+ */
 
 public class FileParameters {
 
-	private final static String sep = System.getProperty("file.separator");
-	private final static String datadir = System.getProperty("user.dir") +  sep + "resources";
-	
-	public final static String fxmlPath = datadir + sep + "testGUI.fxml";
+  private final static String sep = System.getProperty("file.separator");
+  private final static String datadir = System.getProperty("user.dir") + sep + "resources";
+
+  public final static String fxmlPath = datadir + sep + "testGUI.fxml";
 }
