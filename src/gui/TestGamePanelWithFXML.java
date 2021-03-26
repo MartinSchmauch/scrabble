e08c4bcaf07a4cf187cf92bbfecf0eeb889cc1ea
Martin Pascal Schmauch <mschmauc@mail.uni-mannheim.de>
2021-03-26 16:00:55 +0100

    #29 Changed the imports of the TestGamePanelWithFXML in gui

--- src/gui/TestGamePanelWithFXML.java
+++ src/gui/TestGamePanelWithFXML.java
@@ -1,28 +1,26 @@
 package gui;
 
 import java.io.File;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import gui.FileParameters;
 
-//** @author mschmauc
+// ** @author mschmauc
 
-public class TestGamePanelWithFXML extends Application{
-	
-	public static void main(String[] args) {
-		launch();
-	}
-	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		VBox box = FXMLLoader.load(new File(FileParameters.fxmlPath).toURI().toURL());
-		Scene scene1 = new Scene(box);
-		primaryStage.setScene(scene1);
-		primaryStage.show();
-	}
+public class TestGamePanelWithFXML extends Application {
+
+  public static void main(String[] args) {
+    launch();
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    VBox box = FXMLLoader.load(new File(FileParameters.fxmlPath).toURI().toURL());
+    Scene scene1 = new Scene(box);
+    primaryStage.setScene(scene1);
+    primaryStage.show();
+  }
 
 }
