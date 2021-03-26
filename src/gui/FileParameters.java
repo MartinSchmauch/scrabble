51587589a29bfdc3595a9c7322248711f4c61feb
Martin Pascal Schmauch <mschmauc@mail.uni-mannheim.de>
2021-03-26 16:06:00 +0100

    #29 changed fxml path name

--- src/gui/FileParameters.java
+++ src/gui/FileParameters.java
@@ -13,5 +13,5 @@ public class FileParameters {
   private final static String sep = System.getProperty("file.separator");
   private final static String datadir = System.getProperty("user.dir") + sep + "resources";
 
-  public final static String fxmlPath = datadir + sep + "testGUI.fxml";
+  public final static String fxmlPath = datadir + sep + "Test_MainGamePanel_Martin.fxml";
 }
