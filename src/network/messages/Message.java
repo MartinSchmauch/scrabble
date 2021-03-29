5e4b340df96cabda9943d99133f918a1de0632ca
Leon Yannick Urny <lurny@mail.uni-mannheim.de>
2021-03-29 14:16:09 +0200

    #33 setting up message class

---
+++ src/network/messages/Message.java
@@ -0,0 +1,30 @@
+//** author lurny
+package network.messages;
+
+import java.io.Serializable;
+
+import network.messages.MessageType;
+
+public abstract class Message implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private MessageType mType;
+	private String from;
+
+	public Message(MessageType type, String from) {
+		this.mType = type;
+		this.from = new String(from);
+	}
+
+	public MessageType getMessageType() {
+		return this.mType;
+	}
+
+	public String getFrom() {
+		return this.from;
+	}
+	
+	public void setFrom(String name){
+		this.from = name;
+	}	
+}
