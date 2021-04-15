4130d1905f9d5c3b1a84b8c4aee95f74be27be96
Nils Wilhelm Becker <nilbecke@mail.uni-mannheim.de>
2021-04-15 15:26:21 +0200

    #32 Launch now through MainApp class

--- src/game/MainApp.java
+++ src/game/MainApp.java
@@ -1,22 +1,22 @@
 package game;
 
 import gui.LoginScreenFXML;
-import javafx.stage.Stage;
 
-/** @author nilbecke
+/**
+ * @author nilbecke
  * 
  * 
- *This class launches the application
+ *         This class launches the application
  */
 
 public class MainApp {
 
-	public static void main(String[] args) {
-		try {
-			new LoginScreenFXML().start(new Stage());
-		} catch (Exception e) {
-				System.out.println("It is doomed"); 
-		}
-	}
+  public static void main(String[] args) {
+    try {
+      LoginScreenFXML.main(args);
+    } catch (Exception e) {
+      System.out.println("It is doomed");
+    }
+  }
 
 }
