1021c73c49bd894c7e9f2c7d5868bca41a6fba36
Nils Wilhelm Becker <nilbecke@mail.uni-mannheim.de>
2021-05-12 19:41:11 +0200

    #39 Major graphical updates to user statistics screen

--- src/gui/UserStatisticsScreenController.java
+++ src/gui/UserStatisticsScreenController.java
@@ -23,11 +23,26 @@ public class UserStatisticsScreenController implements EventHandler<ActionEvent>
 
   private UserStatisticsScreen uss;
   private Player player;
+  private String[] stats =
+      {"Total Score", "Wins", "Best Word", "Best Turn", "Played Tiles", "Good invested time"};
+  private byte indicator = 0;
 
   @FXML
   private Label nickname;
   @FXML
   private ImageView avatar;
+  @FXML
+  private Label value1;
+  @FXML
+  private Label value2;
+  @FXML
+  private Label key;
+  @FXML
+  private Button next;
+  @FXML
+  private Button prev;
+  @FXML
+  private Button rules;
 
 
   /**
@@ -36,9 +51,10 @@ public class UserStatisticsScreenController implements EventHandler<ActionEvent>
   public void initialize() {
     this.uss = UserStatisticsScreen.getInstance();
     this.player = uss.getPlayer();
-    this.nickname.setText(player.getNickname());
+    setUpLabels();
     this.avatar.setImage(new Image("file:" + FileParameters.datadir + this.player.getAvatar()));
   }
+
 
   @Override
   public void handle(ActionEvent e) {
@@ -49,9 +65,55 @@ public class UserStatisticsScreenController implements EventHandler<ActionEvent>
         s.close();
         new LoginScreen().start(new Stage());
         break;
+      case "rules":
+        OpenExternalScreen
+            .open(System.getProperty("user.dir") + "/src/gui/images/ScrabbleRules.pdf");
+        break;
+      case "prev":
+        updateStatistics(false);
+        break;
+      case "next":
+        updateStatistics(true);
+        break;
       default:
         break;
 
     }
+  }
+
+  /**
+   * This method updates the displayed statistics and is called when the user hits the "next" or
+   * "previous" button.
+   * 
+   * @param increment indicates if the next or previous statistic shoud be shown
+   */
+  public void updateStatistics(boolean increment) {
+    if (increment) {
+      if (indicator < 5) {
+        indicator++;
+      } else {
+        indicator = 0;
+      }
+    } else {
+      if (indicator > 0) {
+        indicator--;
+      } else {
+        indicator = 5;
+      }
+    }
+    this.key.setText(this.stats[indicator]);
+    this.value1.setText("Value1 for " + this.key.getText());
+    this.value2.setText("Value2 for " + this.key.getText());
+  }
+
+  /**
+   * This method sets up all Labels with username and the initial presented statistics.
+   */
+  private void setUpLabels() {
+
+    this.nickname.setText(player.getNickname());
+    this.key.setText(this.stats[indicator]);
+    this.value1.setText("Value1 for " + this.key.getText());
+    this.value2.setText("Value2 for " + this.key.getText());
   }
 }
