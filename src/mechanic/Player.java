567ce8f47bcd1360640d175d0bf77532ffa8b658
Nils Wilhelm Becker <nilbecke@mail.uni-mannheim.de>
2021-05-16 22:04:59 +0200

    #39 revert faulty push

--- src/mechanic/Player.java
+++ src/mechanic/Player.java
@@ -146,7 +146,7 @@ public class Player {
   }
 
   /**
-   * This method adds a List of tileson the playerÂ´s rack.
+   * This method adds a List of tileson the player´s rack.
    * 
    * @author lurny
    */
