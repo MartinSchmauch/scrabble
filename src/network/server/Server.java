0b1258add489fff7f136ba8c54c02c18000b15cc
Luis Dreyer <ldreyer@mail.uni-mannheim.de>
2021-05-30 22:00:42 +0200

    #37 avoid player skip if player leaves

--- src/network/server/Server.java
+++ src/network/server/Server.java
@@ -538,6 +538,7 @@ public class Server {
 
     this.gameState.leaveGame(player);
     this.clients.remove(player);
+    this.gameController.decreaseCurrentPlayerIndex();
     if (this.gameState.getGameRunning() && this.gameState.getAllPlayers().size() < 2) {
       Runnable r = new Runnable() {
         public void run() {
